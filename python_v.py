0809373ed350eea70aefc28cd9ba3d558b19a9e2
rozariwang <hohsuann@gmail.com>
2024-09-20 09:40:29 +0200

    change

--- python_v.py
+++ python_v.py
@@ -2,7 +2,7 @@ import subprocess
 
 try:
     # Run the 'python -V' command with a timeout
-    version = subprocess.run(["python", "-V"], capture_output=True, text=True, timeout=10)  # 10-second timeout
+    version = subprocess.run(["python3", "-V"], capture_output=True, text=True, timeout=10)  # 10-second timeout
     print(version.stdout)
 except subprocess.TimeoutExpired:
     print("The command took too long to execute.")
