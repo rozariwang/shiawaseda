3f977bc239da4f77525d41237b3288ea63912189
rozariwang <hohsuann@gmail.com>
2024-09-21 17:07:37 +0200

    change

--- lsv_cluster_files/run_mamba.sh
+++ lsv_cluster_files/run_mamba.sh
@@ -1,3 +1,5 @@
-nvidia-smi
-nvcc --version
+nvcc --version || echo "nvcc not found"
+ldconfig -p | grep cuda || echo "CUDA libraries not found"
+nvidia-smi || echo "nvidia-smi not found"
+python3 --version
 python /nethome/hhwang/hhwang/shiawaseda/M2_BPE_full_data.py
